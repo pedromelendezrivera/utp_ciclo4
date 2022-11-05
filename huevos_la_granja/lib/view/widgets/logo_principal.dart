3d821542701c11ef0f0bbb716c9ad13000e4b249
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-11-05 16:38:28 -0500

    Se realiza ajustes a la interfaz del login, cambiando posiciones, anchos, altos y bordes para mejorar la experiencia visual del usuario.

--- huevos_la_granja/lib/view/widgets/logo_principal.dart
+++ huevos_la_granja/lib/view/widgets/logo_principal.dart
@@ -2,12 +2,5 @@ import 'package:flutter/material.dart';
 
 const _imageUrl = "assets/images/inicio.png";
 Widget logoPrincipal() {
-  return Column(
-    children: [
-      const SizedBox(
-        height: 40,
-      ),
-      Image.asset(_imageUrl),
-    ],
-  );
+  return Image.asset(_imageUrl);
 }
