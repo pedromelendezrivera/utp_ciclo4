86c0e806afe41e4a486f10bb9cd86efce66d4b4b
Jonathan Pulido <idiqe2@gmail.com>
2022-11-06 13:00:37 -0500

    Se modifica rutas de las imagenes en fondo menu

--- huevos_la_granja/lib/view/widgets/logo_principal.dart
+++ huevos_la_granja/lib/view/widgets/logo_principal.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const _imageUrl = "assets/images/inicio.png";
+const _imageUrl = "assets/images/logo_principal.png";
 Widget logoPrincipal() {
   return Image.asset(_imageUrl);
 }
