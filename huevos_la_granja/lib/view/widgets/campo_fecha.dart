ff92c9921e80e50ca52c3b6936aaba9969413f20
Jonathan Stiven Pulido Ramirez <idiqe2@gmail.com>
2022-11-19 19:39:49 -0500

    Se crea clase para el campo calendario el cual se deja e los widgets generales para su reusabilidad, esta clase recibe como parametro el nombre del campo que se desea ver en el titulo

---
+++ huevos_la_granja/lib/view/widgets/campo_fecha.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+
+class CampoFecha extends StatefulWidget {
+  final String nombre;
+  const CampoFecha({super.key, required this.nombre});
+  @override
+  // ignore: no_logic_in_create_state
+  State<CampoFecha> createState() => _CampoFechaEstado(nombre: nombre);
+}
+
+class _CampoFechaEstado extends State<CampoFecha> {
+  final String nombre;
+  _CampoFechaEstado({
+    required this.nombre,
+  });
+  DateTime date = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return _FormDatePicker(
+      nombre: nombre,
+      date: date,
+      onChanged: (value) {
+        setState(() {
+          date = value;
+        });
+      },
+    );
+  }
+}
+
+class _FormDatePicker extends StatefulWidget {
+  final DateTime date;
+  final String nombre;
+  final ValueChanged<DateTime> onChanged;
+
+  const _FormDatePicker({
+    required this.date,
+    required this.onChanged,
+    required this.nombre,
+  });
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<_FormDatePicker> createState() => _FormDatePickerState(nombre: nombre);
+}
+
+class _FormDatePickerState extends State<_FormDatePicker> {
+  final String nombre;
+  _FormDatePickerState({
+    required this.nombre,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              nombre,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text(
+              intl.DateFormat.yMd().format(widget.date),
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ],
+        ),
+        TextButton(
+          child: const Text('Edit'),
+          onPressed: () async {
+            var newDate = await showDatePicker(
+              context: context,
+              initialDate: widget.date,
+              firstDate: DateTime(1900),
+              lastDate: DateTime(2100),
+            );
+
+            // Don't change the date if the date picker returns null.
+            if (newDate == null) {
+              return;
+            }
+
+            widget.onChanged(newDate);
+          },
+        )
+      ],
+    );
+  }
+}
